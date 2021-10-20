99ce8fd79e1b33c790dd36176c7b5984be68ead7
sigurdmik <sigurd.w.mikalsen@hlkbb.no>
2021-10-20 19:07:21 +0200

    Added create_questions function

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-class Spørsmål:
+import os
+import sys
+
+
+
+class Spørsmål: 
     def __init__(self, spørsmål, alternativer, riktig_svar):
         self.spørsmål = spørsmål
         self.alternativer = alternativer
@@ -12,34 +17,39 @@ class Spørsmål:
 
     def sjekk_svar(self):
         avgitt_svar = int(input())
-        if avgitt_svar == self.riktig_svar:
+        riktig_svar = int(self.riktig_svar)
+        if avgitt_svar == (riktig_svar+1):                                          #Bruker riktig_svar +1 fordi jeg vil at brukeren skal svare med spm nummeret ikke index
             return 1
         else:
             return 0
 
 
-spm1 = Spørsmål("Hvordan by er en hovedstad?", ("Stavanger", "Harstad", "Oslo"), 3)
-spm2 = Spørsmål("Hvilke tall er minst?", ("4", "5", "6"), 1)
-spm3 = Spørsmål("Hvilke passer ikke inn?", ("Ja", "Nei", "Kanskje", "Hade"), 4)
-spm4 = Spørsmål("Hvilke passer ikke inn?", ("Ja", "Nei", "Kanskje", "Hade"), 4)
 
+                                                                                    #Listen som vil inneholde alle spørsmålene etter create_questions() har kjørt
+questions = []
 
-spm = [spm1, spm2, spm3, spm4] 
+def create_questions():
+    fila = (open(os.path.join(sys.path[0], "sporsmaalsfil.txt"), "r"))              #Åpner filen
+    fil = [] 
+    for linje in fila:                                                              #Splitter filen opp i linjer siden det er 1 spørsmål for hver linje
+        fil.append(linje)
+        alternativer = []
+    for i in range(len(fil)):                                                       #Tar for meg en og en linje
+        rad = fil[i].split(":")                                                     #Splitter linjen opp med seperatoren :
+        alternativer = rad[2].strip().strip("[").strip("]").split(",")              
+        questions.append(Spørsmål(rad[0], alternativer, rad[1]))                    #Bruker klassen Spørsmål og sender de ferdige spørsmålene inn i listen questions
 
-
-
-def spill():
+def spill():                                                                        #Funksjonen som kjører selve spillet
     poeng = 0
-    full_score = len(spm)   
-    for i in range(len(spm)):
-        print(spm[i])
-        if spm[i].sjekk_svar() == 1:
+    full_score = len(questions)
+    for i in range(full_score):
+        print(questions[i])
+        if questions[i].sjekk_svar() == 1:
             poeng += 1
         print("\n")
     print(f"Du fikk {poeng}/{full_score} riktige")
 
 
 if __name__ == '__main__':
+    create_questions()
     spill()
-
-        
